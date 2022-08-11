439ad79bb3908f7fbac5b57e65c101eedd4c1de3
Natasha <n.kurseneva@gmail.com>
2022-08-11 06:40:09 +0300

    Ass Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿
+//Console.WriteLine("Введите число: ");
+//int a = Console.Read();
+int a = 9;
+
+if(a % 2 == 0)
+{
+    Console.Write("Число четное");
+}
+else{
+    Console.Write("Число нечетное");
+}

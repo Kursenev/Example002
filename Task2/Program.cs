76b2b6df3f8909eb934f359ed78b678504bfeee1
Natasha <n.kurseneva@gmail.com>
2022-08-11 06:29:15 +0300

    Add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 10;
+int b = 7;
+int c = 15;
+int max = a;
+
+if(max > b)
+{
+   if(max > c)
+   {
+    Console.Write("Max = ");
+    Console.WriteLine(max);
+   }
+   else{
+    max = c;
+   }
+    Console.Write("Max = ");
+    Console.WriteLine(max);
+}
+else{
+    max = b;
+    Console.Write("Min = ");
+    Console.WriteLine(max);
+}
+
